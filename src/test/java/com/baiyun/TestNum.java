d707236e360bf7476f265679b0c945b54fafed66
smileby <337531158@qq.com>
2021-01-21 12:39:09 +0800

    Delete TestNum.java

--- src/test/java/com/baiyun/TestNum.java
+++
@@ -1,9 +0,0 @@
-package com.baiyun;
-
-public class TestNum {
-    public static void main(String[] args) {
-        System.out.println("a" + 1);
-        System.out.println('a' + 1);
-        System.out.println('A' + 1);
-    }
-}
